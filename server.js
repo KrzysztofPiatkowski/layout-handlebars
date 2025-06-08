bb9d6460df6de55a647e5da8da55ad1cd676b7b8
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-06-08 14:02:46 +0200

    Add contact form with file upload and validation

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+const multer = require('multer');
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  }
+});
+
 const app = express();
 app.engine('hbs', hbs({
   extname: 'hbs',
@@ -12,6 +26,8 @@ app.engine('hbs', hbs({
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -35,6 +51,20 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.render('history');
+});
+
+app.post('/contact/send-message', upload.single('design'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  const file = req.file;
+
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, fileName: file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
